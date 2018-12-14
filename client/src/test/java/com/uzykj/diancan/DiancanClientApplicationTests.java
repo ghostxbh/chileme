38d33e5647c37430d65c8af6dabeb26e13857f0d
ghostxbh <405963225@qq.com>
2018-12-14 14:39:30 +0800

    点餐系统目录提交

---
+++ client/src/test/java/com/uzykj/diancan/DiancanClientApplicationTests.java
@@ -0,0 +1,17 @@
+package com.uzykj.diancan;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DiancanClientApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
---
+++ server/src/main/java/com/uzykj/diancan/DiancanServerApplication.java
@@ -0,0 +1,14 @@
+package com.uzykj.diancan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiancanServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiancanServerApplication.class, args);
+	}
+
+}
+
