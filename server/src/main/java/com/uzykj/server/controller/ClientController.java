313647661ab0a4f5d20ad65973b7480fd6a0b3a6
ghostxbh <ghostxbh@hotmail.com>
2018-12-16 21:40:43 +0800

    部分修改

--- server/src/main/java/com/uzykj/server/controller/ClientController.java
+++ server/src/main/java/com/uzykj/server/controller/ClientController.java
@@ -1,18 +1,10 @@
 package com.uzykj.server.controller;
 
 
-import com.uzykj.client.ProductClient;
-import com.uzykj.common.DecreaseStockInput;
-import com.uzykj.common.ProductInfoOutput;
-import com.uzykj.server.pojo.dto.CartDTO;
-import com.uzykj.server.pojo.model.Product;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+
 
 import java.util.Arrays;
-import java.util.List;
+
 
 
 /**
